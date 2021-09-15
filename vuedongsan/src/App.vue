5a9a52023e7eafe3bdb010ed1a203f52cb72c788
dionysus0214 <hiba0214@naver.com>
2021-09-15 11:54:26 +0900

    이벤트리스너

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -3,8 +3,18 @@
     <a v-for="menu in menus" :key="menu">{{ menu }}</a>
   </div>
   <div>
-    <h4 v-for="product in products" :key="product">{{ product }}</h4>
-    <p v-for="price in prices" :key="price">{{ price }}</p>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ prices[0] }}</p>
+    <button @click="calcuateCount">허위매물신고</button>
+    <span>신고수: {{ count }}</span>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ prices[1] }}</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ prices[2] }}</p>
   </div>
 </template>
 
@@ -15,7 +25,13 @@ export default {
     return {
       menus: ['Home', 'Shop', 'About'],
       products: ['역삼동 원룸', '천호동 원룸', '서초동 원룸'],
-      prices: ['50만원', '40만원', '70만원']
+      prices: ['50만원', '40만원', '70만원'],
+      count: 0,
+    }
+  },
+  methods: {
+    calcuateCount() {
+      this.count++; // 함수 안에서 데이터 쓸 땐 this.데이터명
     }
   },
   components: {
@@ -30,6 +46,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 
 .menu {
