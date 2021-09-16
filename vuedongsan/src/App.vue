c23d944ba5d5a667e69d957bc47538627fb92173
dionysus0214 <hiba0214@naver.com>
2021-09-17 00:00:20 +0900

    모달창

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -2,19 +2,33 @@
   <div class="menu">
     <a v-for="menu in menus" :key="menu">{{ menu }}</a>
   </div>
-  <div>
-    <h4>{{ products[0] }}</h4>
-    <p>{{ prices[0] }}</p>
-    <button @click="calcuateCount">허위매물신고</button>
-    <span>신고수: {{ count }}</span>
+  <div v-if="modal == true" class="black-bg">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button @click="modal = false">닫기</button>
+    </div>
   </div>
   <div>
+    <img src="./assets/room0.jpg" class="room-img" />
+    <h4 @click="modal = true">{{ products[0] }}</h4>
+    <p>{{ prices[0] }}</p>
+    <button @click="count[0]++">허위매물신고</button>
+    <span>신고수: {{ count[0] }}</span>
+  </div>
+  <div>
+    <img src="./assets/room1.jpg" class="room-img" />
     <h4>{{ products[1] }}</h4>
     <p>{{ prices[1] }}</p>
+    <button @click="count[1]++">허위매물신고</button>
+    <span>신고수: {{ count[1] }}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room-img" />
     <h4>{{ products[2] }}</h4>
     <p>{{ prices[2] }}</p>
+    <button @click="count[2]++">허위매물신고</button>
+    <span>신고수: {{ count[2] }}</span>
   </div>
 </template>
 
@@ -26,12 +40,8 @@ export default {
       menus: ['Home', 'Shop', 'About'],
       products: ['역삼동 원룸', '천호동 원룸', '서초동 원룸'],
       prices: ['50만원', '40만원', '70만원'],
-      count: 0,
-    }
-  },
-  methods: {
-    calcuateCount() {
-      this.count++; // 함수 안에서 데이터 쓸 땐 this.데이터명
+      count: [0, 0, 0],
+      modal: false,
     }
   },
   components: {
@@ -49,6 +59,29 @@ export default {
   margin-top: 60px;
 }
 
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 .menu {
   background: darkslateblue;
   padding: 15px;
@@ -59,4 +92,13 @@ export default {
   color: white;
   padding: 10px;
 }
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
 </style>
+
+// 동적인 UI 만드는 법
+// 1. UI의 현재 상태를 데이터로 저장
+// 2. 데이터에 따라 UI가 어떻게 보일지 작성
