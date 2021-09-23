d0469a5a87b67d8f1a7f67459d629823c834dc80
dionysus0214 <hiba0214@naver.com>
2021-09-23 22:44:16 +0900

    블로그 글 목록 데이터 바인딩

--- vuelog/src/App.vue
+++ vuelog/src/App.vue
@@ -1,11 +1,32 @@
 <template>
-  <button type="button" class="btn btn-primary">Primary</button>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Vuelog</a>
+    </div>
+  </nav>
+
+  <div class="container mt-4">
+    <p>vue로 만들었음</p>
+  </div>
+
+  <List
+    :blog="blog"
+  />
 </template>
 
 <script>
+import blog from './blog.js';
+import List from './components/List.vue';
+
 export default {
   name: 'App',
+  data() {
+    return {
+      blog: blog,
+    }
+  },
   components: {
+    List,
   }
 }
 </script>
---
+++ vuelog/src/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h5>{{ blog[0].title }}</h5>
+    <p>{{ blog[0].date }}</p>
+  </div>
+  <div>
+    <h5>{{ blog[1].title }}</h5>
+    <p>{{ blog[1].date }}</p>
+  </div>
+  <div>
+    <h5>{{ blog[2].title }}</h5>
+    <p>{{ blog[2].date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    blog: Array, // [로 시작하면 array, {로 시작하면 object
+  }
+}
+</script>
+
+<style>
+
+</style>
