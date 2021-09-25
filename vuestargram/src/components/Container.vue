9861b2e19fcea65b13ba03b642aa6cb4d75ebdee
dionysus0214 <hiba0214@naver.com>
2021-09-25 21:37:36 +0900

    vuestargram 프로젝트 생성, 레이아웃 만들기

---
+++ vuestargram/src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Post/>
+    <Post/>
+    <Post/>
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+  components: {
+    Post,
+  }
+}
+</script>
+
+<style>
+
+</style>
